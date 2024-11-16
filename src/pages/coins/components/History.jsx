383875bde76a021ad31734c713fb99ce23714b43
kurbanovsaipy <kurbanovsajp@gmail.com>
2024-11-16 14:06:30 +0300

    upd

--- src/pages/coins/components/History.jsx
+++ src/pages/coins/components/History.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import HistoryItem from './HistoryItem';
 
 const History = () => {
     return (
-        <div>
-            
+        <div className='coins_history'>
+            <h3>История игр</h3>
+
+            <div className="hostory_list">
+                <div className='history_item head'>
+                    <div className="player">Игрок</div>
+                    <div className="bet">Ставка</div>
+                    <div className="hit">Попаданий</div>
+                    <div className="coef">Коэфф.</div>
+                    <div className="win">Выигрыш</div>
+                    <div></div>
+                </div>
+                <HistoryItem />
+                <HistoryItem />
+                <HistoryItem />
+
+            </div>
         </div>
     );
 };
